e07e63ac8653a593aa5dae05c9da543954046a2b
Anastasiia <adermahnastya@gmail.com>
2021-11-29 13:49:37 +0200

    injected ThredPool in CarDirector

--- src/main/java/com/pz35/carfactory/logic/CarDirector.java
+++ src/main/java/com/pz35/carfactory/logic/CarDirector.java
@@ -4,18 +4,21 @@ import com.pz35.carfactory.entities.Accessory;
 import com.pz35.carfactory.entities.Car;
 import com.pz35.carfactory.entities.CarBody;
 import com.pz35.carfactory.entities.Engine;
+import com.pz35.carfactory.threadpool.ThreadPool;
 
 public class CarDirector {
-    Warehouse<Accessory> accessoryStorage;
-    Warehouse<Engine> engineStorage;
-    Warehouse<CarBody> bodyStorage;
+    private Warehouse<Accessory> accessoryStorage;
+    private Warehouse<Engine> engineStorage;
+    private Warehouse<CarBody> bodyStorage;
 
-    CarWarehouse carStorage;
+    private CarWarehouse carStorage;
 
-    boolean shouldStop = false;
-    long waitTime;
-    int generatedParticlesCount = 0;
-    boolean first = true;
+    private boolean shouldStop = false;
+    private long waitTime;
+    private int generatedParticlesCount = 0;
+    private boolean first = true;
+
+    private static ThreadPool threadPool;
     private class CreateCar implements Runnable {
         public void run() {
             if (accessoryStorage.tryGet() &&
@@ -35,15 +38,51 @@ public class CarDirector {
                        Warehouse<Engine> ew,
                        Warehouse<CarBody> bw,
                        CarWarehouse cw,
-                       long waitTime) {
+                       long waitTime,
+                       ThreadPool tp) {
         accessoryStorage = aw;
         engineStorage = ew;
         bodyStorage = bw;
         carStorage = cw;
 
         this.waitTime = waitTime;
+
+        threadPool = tp;
     }
 
+    static public void init()
+    {
+        threadPool.start();
+    }
+    public void run() {
+        while (!shouldStop) {
+            while (!accessoryStorage.tryGet() ||
+                    !engineStorage.tryGet() ||
+                    !bodyStorage.tryGet())
+            {
+                ;
+            }
+            Runnable r = new CreateCar();
+            generatedParticlesCount++;
+            threadPool.enqueue(r);
 
+            synchronized (Thread.currentThread()) {
+                try {
+                    Thread.currentThread().wait(waitTime);
+                } catch (InterruptedException e) {
+                    shouldStop = true;
+                }
+            }
+        }
+    }
+
+    public void terminate() {
+        shouldStop = true;
+        threadPool.setShouldStop();
+        threadPool.interrupt();
+        Thread.currentThread().interrupt();
+        threadPool.dequeue();
+    }
+    public int getNumberOfCreatedParticles() { return generatedParticlesCount; }
 
 }
